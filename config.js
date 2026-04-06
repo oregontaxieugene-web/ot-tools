c5c8e821a9145797aa0f1b8695e0c48bd6057482
oregontaxieugene-web <oregontaxieugene@gmail.com>
2026-04-05 20:46:41 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ window.ENV = {
     SUPABASE_URL: 'https://ioofrqvykvgshqybzjiu.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlvb2ZycXZ5a3Znc2hxeWJ6aml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NjcxNzcsImV4cCI6MjA5MDU0MzE3N30.E3Kd88OXGGIc6GRmaVJaJwTilLuPksqyYPYSBEyECUQ',
     CLOUDFLARE_WORKER_URL: 'http://ot-tools.com'
+};
